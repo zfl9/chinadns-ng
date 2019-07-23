da9743969b61a5c246dacaccd80cdccbb40c3c98
zfl9 <zfl9.com@gmail.com>
2019-07-23 19:57:31 +0800

    update netutils

--- netutils.c
+++ netutils.c
@@ -30,18 +30,20 @@ struct nfgenmsg {
     __be16  res_id;         /* resource id */
 };
 
-#define MSGBUFFER_MAXLEN 1024
+#define MSGBUFFER_MAXLEN 512
 
 /* static global variable declaration */
 static int          g_ipset_socket = -1;
-static size_t       g_ipset_nlmsg_seq = 0;
+static __u32        g_ipset_nlmsg_seq = 0;
 static char         g_ipset_setname4[IPSET_MAXNAMELEN] = {0};
 static char         g_ipset_setname6[IPSET_MAXNAMELEN] = {0};
 static char         g_ipset_sendbuffer4[MSGBUFFER_MAXLEN] = {0};
 static char         g_ipset_sendbuffer6[MSGBUFFER_MAXLEN] = {0};
 static char         g_ipset_recvbuffer[MSGBUFFER_MAXLEN] = {0};
-static ipv4_addr_t *g_ipset_ipv4addr_memory_pointer = NULL;
-static ipv6_addr_t *g_ipset_ipv6addr_memory_pointer = NULL;
+static ipv4_addr_t *g_ipset_ipv4addr_ptr = NULL;
+static ipv6_addr_t *g_ipset_ipv6addr_ptr = NULL;
+static __u32       *g_ipset_nlmsg4_seq_ptr = NULL;
+static __u32       *g_ipset_nlmsg6_seq_ptr = NULL;
 
 /* create a udp socket (AF_INET) */
 int new_udp4_socket(void) {
@@ -138,6 +140,8 @@ static void ipset_prebuild_nlmsg(bool is_ipv4) {
     netlink_msg->nlmsg_type = (NFNL_SUBSYS_IPSET << 8) | IPSET_CMD_TEST;
     netlink_msg->nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK;
     netlink_msg->nlmsg_seq = g_ipset_nlmsg_seq; // should be incremented
+    if (is_ipv4) g_ipset_nlmsg4_seq_ptr = &netlink_msg->nlmsg_seq; // ptr for store ipv4 nlmsg seq
+    if (!is_ipv4) g_ipset_nlmsg6_seq_ptr = &netlink_msg->nlmsg_seq; // ptr for store ipv6 nlmsg seq
     netlink_msg->nlmsg_pid = getpid();
 
     /* netfilter msg */
@@ -178,8 +182,8 @@ static void ipset_prebuild_nlmsg(bool is_ipv4) {
     struct nlattr *ipset_ip_attr = buffer + netlink_msg->nlmsg_len;
     ipset_ip_attr->nla_len = NLMSG_ALIGN(sizeof(struct nlattr)) + (is_ipv4 ? sizeof(ipv4_addr_t) : sizeof(ipv6_addr_t));
     ipset_ip_attr->nla_type = (is_ipv4 ? IPSET_ATTR_IPADDR_IPV4 : IPSET_ATTR_IPADDR_IPV6) | NLA_F_NET_BYTEORDER;
-    g_ipset_ipv4addr_memory_pointer = (void *)&ipset_ip_attr->nla_type + sizeof(ipset_ip_attr->nla_type);
-    g_ipset_ipv6addr_memory_pointer = (void *)&ipset_ip_attr->nla_type + sizeof(ipset_ip_attr->nla_type);
+    if (is_ipv4) g_ipset_ipv4addr_ptr = (void *)&ipset_ip_attr->nla_type + sizeof(ipset_ip_attr->nla_type); // ptr for store ipv4 addr
+    if (!is_ipv4) g_ipset_ipv6addr_ptr = (void *)&ipset_ip_attr->nla_type + sizeof(ipset_ip_attr->nla_type); // ptr for store ipv6 addr
     ipset_ip_nestedattr->nla_len += NLMSG_ALIGN(ipset_ip_attr->nla_len); // update ipset_ip attrlen
     ipset_data_nestedattr->nla_len += NLMSG_ALIGN(ipset_ip_attr->nla_len); // update ipset_data attrlen
     netlink_msg->nlmsg_len += NLMSG_ALIGN(ipset_ip_attr->nla_len); // update netlink msglen
@@ -196,11 +200,33 @@ void ipset_init_nlsocket(const char *ipset_name4, const char *ipset_name6) {
 
 /* check given ipaddr is exists in set */
 bool ipset_addr4_is_exists(ipv4_addr_t addr) {
-    // TODO
-    return false;
+    *g_ipset_ipv4addr_ptr = addr;
+    if (send(g_ipset_socket, g_ipset_sendbuffer4, ((struct nlmsghdr *)g_ipset_sendbuffer4)->nlmsg_len, 0) < 0) {
+        LOGERR("[ipset_addr4_is_exists] failed to send netlink msg to kernel: (%d) %s", errno, strerror(errno));
+        return false;
+    }
+    *g_ipset_nlmsg4_seq_ptr = ++g_ipset_nlmsg_seq;
+
+    if (recv(g_ipset_socket, g_ipset_recvbuffer, MSGBUFFER_MAXLEN, 0) < 0) {
+        LOGERR("[ipset_addr4_is_exists] failed to recv netlink msg from kernel: (%d) %s", errno, strerror(errno));
+        return false;
+    }
+    struct nlmsgerr *netlink_errmsg = NLMSG_DATA(g_ipset_recvbuffer);
+    return netlink_errmsg->error == 0 ? true : false;
 }
 
 bool ipset_addr6_is_exists(ipv6_addr_t addr) {
-    // TODO
-    return false;
+    memcpy(g_ipset_ipv6addr_ptr, addr, sizeof(ipv6_addr_t));
+    if (send(g_ipset_socket, g_ipset_sendbuffer6, ((struct nlmsghdr *)g_ipset_sendbuffer6)->nlmsg_len, 0) < 0) {
+        LOGERR("[ipset_addr6_is_exists] failed to send netlink msg to kernel: (%d) %s", errno, strerror(errno));
+        return false;
+    }
+    *g_ipset_nlmsg6_seq_ptr = ++g_ipset_nlmsg_seq;
+
+    if (recv(g_ipset_socket, g_ipset_recvbuffer, MSGBUFFER_MAXLEN, 0) < 0) {
+        LOGERR("[ipset_addr6_is_exists] failed to recv netlink msg from kernel: (%d) %s", errno, strerror(errno));
+        return false;
+    }
+    struct nlmsgerr *netlink_errmsg = NLMSG_DATA(g_ipset_recvbuffer);
+    return netlink_errmsg->error == 0 ? true : false;
 }
