50158581df2fbe92268a01dd04a9e8a0bfbfdb4f
zfl9 <zfl9.com@gmail.com>
2019-07-25 20:35:03 +0800

    update chinadns.c

--- chinadns.c
+++ chinadns.c
@@ -44,30 +44,35 @@ static char         g_socket_buffer[SOCKBUFF_MAXSIZE]   = {0};
 static uint16_t     g_current_message_id                = 1;
 static hashmap_t   *g_message_id_hashmap                = NULL;
 
+/* print command help information */
 static void print_command_help(void) {
     printf("usage: chinadns-ng <options...>. the existing options are as follows:\n"
-           " -b, --bind-addr <addr>               listen address. default: 127.0.0.1\n" 
+           " -b, --bind-addr <addr>               listen address, default: 127.0.0.1\n" 
            " -l, --bind-port <port>               listen port number, default: 65353\n"
-           " -c, --china-dns <ip:port[,ip:port]>  upstream dns server, chinadns list\n"
-           " -t, --trust-dns <ip:port[,ip:port]>  upstream dns server, trustdns list\n"
-           " -4, --ipset-name4 <ipset-setname4>   ipset chnroute cidr setname (ipv4)\n"
-           " -6, --ipset-name6 <ipset-setname6>   ipset chnroute cidr setname (ipv6)\n"
-           " -r, --reuse-port                     set SO_REUSEPORT for listen socket\n"
-           " -v, --verbose                        print the verbose log when running\n"
-           " -V, --version                        print chinadns-ng version and exit\n"
-           " -h, --help                           print chinadns-ng help msg and exit\n"
+           " -c, --china-dns <ip:port[,ip:port]>  china dns server, default: <114DNS>\n"
+           " -t, --trust-dns <ip:port[,ip:port]>  trust dns server, default: <GoogleDNS>\n"
+           " -4, --ipset-name4 <ipset-setname4>   ipset ipv4 set name, default: chnroute\n"
+           " -6, --ipset-name6 <ipset-setname6>   ipset ipv6 set name, default: chnroute6\n"
+           " -r, --reuse-port                     enable SO_REUSEPORT, default: <disabled>\n"
+           " -v, --verbose                        print the verbose log, default: <disabled>\n"
+           " -V, --version                        print `chinadns-ng` version number and exit\n"
+           " -h, --help                           print `chinadns-ng` help information and exit\n"
+           "bug report: https://github.com/zfl9/chinadns-ng. email: zfl9.com@gmail.com (Otokaze)\n"
     );
 }
 
+/* parse and check command arguments */
 static void parse_command_args(int argc, char *argv[]) {
     // TODO
 }
 
-static void handle_local_packet() {
+/* handle local socket readable event */
+static void handle_local_packet(void) {
     // TODO
 }
 
-static void handle_remote_packet() {
+/* handle remote socket readable event */
+static void handle_remote_packet(int index) {
     // TODO
 }
 
