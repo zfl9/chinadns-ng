b12658f6242bf84bf9203e230abf7383c0059e1c
zfl9 <zfl9.com@gmail.com>
2019-07-28 00:06:25 +0800

    add chnroute update script

---
+++ update-chnroute6.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "create chnroute6 hash:net family inet6" >chnroute6.ipset
+reserved_ipaddrs=(
+    ::/128
+    ::1/128
+    ::ffff:0:0/96
+    ::ffff:0:0:0/96
+    64:ff9b::/96
+    100::/64
+    2001::/32
+    2001:20::/28
+    2001:db8::/32
+    2002::/16
+    fc00::/7
+    fe80::/10
+    ff00::/8
+)
+for reserved_ipaddr in "${reserved_ipaddrs[@]}"; do echo "add chnroute6 $reserved_ipaddr" >>chnroute6.ipset; done
+curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep CN | grep ipv6 | awk -F'|' '{printf("add chnroute6 %s/%d\n", $4, $5)}' >>chnroute6.ipset
