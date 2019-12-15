edb7ff7e394a2e1de30b386b4fce2a215d7ab13e
hiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2019-12-15 09:44:29 +0800

    Update dnsutils.h
    修复 ERR: [dns_qheader_check] this is a query packet, but header->qr != 0

--- dnsutils.h
+++ dnsutils.h
@@ -14,10 +14,40 @@
 /* example: "www.example.com", length = 16 (including '\0') */
 #define DNS_DOMAIN_NAME_MAXLEN 254 /* eg: char namebuf[DNS_DOMAIN_NAME_MAXLEN] */
 
+
+#ifndef __BYTE_ORDER
+#	if defined(_BYTE_ORDER)
+#		define __BYTE_ORDER _BYTE_ORDER
+#		define __LITTLE_ENDIAN _LITTLE_ENDIAN
+#		define __BIG_ENDIAN _BIG_ENDIAN
+#	elif defined(BYTE_ORDER)
+#		define __BYTE_ORDER BYTE_ORDER
+#		define __LITTLE_ENDIAN LITTLE_ENDIAN
+#		define __BIG_ENDIAN BIG_ENDIAN
+#	elif defined(__BYTE_ORDER__)
+#		define __BYTE_ORDER __BYTE_ORDER__
+#		define __LITTLE_ENDIAN __LITTLE_ENDIAN__
+#		define __BIG_ENDIAN __BIG_ENDIAN__
+#	elif (defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN)) || \
+			defined(WORDS_LITTLEENDIAN)
+#		define __BYTE_ORDER 1
+#		define __LITTLE_ENDIAN 1
+#		define __BIG_ENDIAN 0
+#	elif (!defined(_LITTLE_ENDIAN) && defined(_BIG_ENDIAN)) || \
+			defined(WORDS_BIGENDIAN)
+#		define __BYTE_ORDER 0
+#		define __LITTLE_ENDIAN 1
+#		define __BIG_ENDIAN 0
+#	else
+#		error "__BYTE_ORDER is not defined."
+#	endif
+#endif
+
+
 /* dns header structure (fixed length) */
 typedef struct __attribute__((packed)) {
     uint16_t id; // id of message
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#if __BYTE_ORDER == __BIG_ENDIAN
     uint8_t  qr:1; // query=0; response=1
     uint8_t  opcode:4; // standard-query=0, etc.
     uint8_t  aa:1; // is authoritative answer, set by server
@@ -26,7 +56,7 @@ typedef struct __attribute__((packed)) {
     uint8_t  ra:1; // is recursion available, set by server
     uint8_t  z:3; // reserved bits set to zero
     uint8_t  rcode:4; // response code: no-error=0, etc.
-#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
     uint8_t  rd:1; // is recursion desired, set by client
     uint8_t  tc:1; // message is truncated, set by server
     uint8_t  aa:1; // is authoritative answer, set by server
