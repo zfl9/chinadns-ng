2840ec7d19743b1b1116b2c808a3898add42a03c
zfl9 <zfl9.com@gmail.com>
2020-04-04 14:36:18 +0800

    rename sock_port_t, inetN_skaddr_t

--- update-chnlist.sh
+++ update-chnlist.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -o errexit
+set -o pipefail
 url='https://raw.github.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
 data=$(curl -4sSkL "$url") || { echo "download failed, exit-code: $?"; exit 1; }
 echo "$data" | awk -F/ '{print $2}' | sort | uniq >chnlist.txt
