31108de4b29355dd27626ee692f4e31dcaf8d2c6
zfl9 <zfl9.com@gmail.com>
2020-04-04 21:31:47 +0800

    update chinadns.c

--- chinadns.c
+++ chinadns.c
@@ -374,7 +374,7 @@ static void handle_local_packet(void) {
     context->trustdns_buf = NULL;
     context->chinadns_got = false;
     context->dnlmatch_ret = dnlmatch_ret;
-    memcpy(&context->source_addr, &source_addr, source_addrlen);
+    memcpy(&context->source_addr, &source_addr, sizeof(source_addr));
     MYHASH_ADD(g_query_context_hashtbl, context, &context->unique_msgid, sizeof(context->unique_msgid));
 }
 
