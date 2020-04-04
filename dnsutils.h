d98f559f70e69c55c6d7928c08f998b5f528717f
zfl9 <zfl9.com@gmail.com>
2020-04-04 19:34:10 +0800

    update dnsutils.h

--- dnsutils.h
+++ dnsutils.h
@@ -9,7 +9,7 @@
 #undef _GNU_SOURCE
 
 #if !(defined(__BYTE_ORDER) && defined(__LITTLE_ENDIAN) && defined(__BIG_ENDIAN))
-    #error "__BYTE_ORDER or __LITTLE_ENDIAN or __BIG_ENDIAN not defined"
+  #error "__BYTE_ORDER or __LITTLE_ENDIAN or __BIG_ENDIAN not defined"
 #endif
 
 /* dns packet max size (in bytes) */
@@ -20,7 +20,7 @@
 #define DNS_DOMAIN_NAME_MAXLEN 254 /* eg: char namebuf[DNS_DOMAIN_NAME_MAXLEN] */
 
 /* dns header structure (fixed length) */
-typedef struct __attribute__((packed)) {
+typedef struct {
     uint16_t id; // id of message
 #if __BYTE_ORDER == __BIG_ENDIAN
     uint8_t  qr:1; // query=0; response=1
@@ -47,24 +47,24 @@ typedef struct __attribute__((packed)) {
     uint16_t answer_count; // answer record count
     uint16_t authority_count; // authority record count
     uint16_t additional_count; // additional record count
-} dns_header_t;
+} __attribute__((packed)) dns_header_t;
 
 /* fixed length of query structure */
-typedef struct __attribute__((packed)) {
+typedef struct {
     // field qname; variable length
     uint16_t qtype; // query type: A/AAAA/CNAME/MX, etc.
     uint16_t qclass; // query class: internet=0x0001
-} dns_query_t;
+} __attribute__((packed)) dns_query_t;
 
 /* fixed length of record structure */
-typedef struct __attribute__((packed)) {
+typedef struct {
     // field rname; variable length
     uint16_t rtype; // record type: A/AAAA/CNAME/MX, etc.
     uint16_t rclass; // record class: internet=0x0001
     uint32_t rttl; // record ttl value (in seconds)
     uint16_t rdatalen; // record data length
     uint8_t  rdataptr[]; // record data pointer (sizeof=0)
-} dns_record_t;
+} __attribute__((packed)) dns_record_t;
 
 /* check a dns query packet, `name_buf` used to get domain name */
 bool dns_query_check(const void *packet_buf, ssize_t packet_len, char *name_buf);
