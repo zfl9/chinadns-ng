f55ae5c39e64b31efef5c8ffb403b034eaebf4b4
pexcn <pexcn97@gmail.com>
2023-02-18 14:27:06 +0800

    update

--- docker/chinadns-ng.sh
+++ docker/chinadns-ng.sh
@@ -1,72 +1,81 @@
 #!/bin/sh
 
+# shellcheck disable=SC2155,SC3043
+
 info() {
   local green='\e[0;32m'
   local clear='\e[0m'
-  local time=$(date '+%Y-%m-%d %T')
+  local time="$(date '+%Y-%m-%d %T')"
   printf "${green}[${time}] [INFO]: ${clear}%s\n" "$*"
 }
 
 warn() {
   local yellow='\e[1;33m'
   local clear='\e[0m'
-  local time=$(date '+%Y-%m-%d %T')
+  local time="$(date '+%Y-%m-%d %T')"
   printf "${yellow}[${time}] [WARN]: ${clear}%s\n" "$*" >&2
 }
 
 error() {
   local red='\e[0;31m'
   local clear='\e[0m'
-  local time=$(date '+%Y-%m-%d %T')
+  local time="$(date '+%Y-%m-%d %T')"
   printf "${red}[${time}] [ERROR]: ${clear}%s\n" "$*" >&2
 }
 
-_update_list() {
+_update_rule() {
   local url="$1"
-  local file="$(basename $url)"
+  local file="$(basename "$url")"
   local dir="/etc/chinadns-ng"
-  local lines="$(wc -l < ${dir}/${file})"
-  curl -sSL --create-dirs --output-dir $dir -O $url
-  info "update ${file}: ${lines} -> $(wc -l < ${dir}/${file})"
+  local lines="$(wc -l < "${dir}/${file}")"
+  curl -sSL --create-dirs --output-dir $dir -O "$url" || return 1
+  info "update ${file}: ${lines} -> $(wc -l < "${dir}/${file}")"
 }
 
-update_lists() {
-  _update_list https://github.com/pexcn/daily/raw/gh-pages/chnroute/chnroute.txt
-  _update_list https://github.com/pexcn/daily/raw/gh-pages/chnroute/chnroute6.txt
-  _update_list https://github.com/pexcn/daily/raw/gh-pages/gfwlist/gfwlist.txt
-  _update_list https://github.com/pexcn/daily/raw/gh-pages/chinalist/chinalist.txt
-  # TODO: cannot restart chinadns-ng if download fails
-  # update_list xxx || return 1 ?
+update_rules() {
+  _update_rule https://github.com/pexcn/daily/raw/gh-pages/chnroute/chnroute.txt || return 1
+  _update_rule https://github.com/pexcn/daily/raw/gh-pages/chnroute/chnroute6.txt || return 1
+  _update_rule https://github.com/pexcn/daily/raw/gh-pages/gfwlist/gfwlist.txt || return 1
+  _update_rule https://github.com/pexcn/daily/raw/gh-pages/chinalist/chinalist.txt || return 1
+}
+
+stop_process() {
+  kill "$(pidof chinadns-ng)"
+  info "terminate chinadns-ng processes."
+
+  # ensure child process terminate completely, avoid <defunct> processes
+  sleep 5
 }
 
 restart_process() {
-  kill $(pidof chinadns-ng)
-  sleep 5
+  stop_process
+
+  # TODO: move `&` to invoker?
   chinadns-ng "$@" &
 }
 
 graceful_stop() {
-  kill $(pidof chinadns-ng)
+  stop_process
 
-  # make sure signal can delivered to child process, avoid <defunct> processes
-  sleep 5
   # exit infinite loop
   exit 0
 }
 
 start_chinadns_ng() {
-  trap 'graceful_stop' SIGTERM SIGINT
+  trap 'graceful_stop' TERM INT
 
-  chinadns-ng "$@" &
-
-  while true; do
-    sleep 10
-    update_lists
-    restart_process
-    #if update_lists; then
-    #  restart_process
-    #fi
-  done
+  if [ "$RULES_UPDATE_INTERVAL" = 0 ]; then
+    # try replace to: `exec chinadns-ng "$@"`
+    # need test `docker stop ...` and `docker-compose up, then ctrl + c`
+    chinadns-ng "$@" &
+    wait
+  else
+    chinadns-ng "$@" &
+    while true; do
+      sleep "$RULES_UPDATE_INTERVAL"
+      update_rules && restart_process "$@"
+    done
+  fi
 }
 
 start_chinadns_ng "$@"
