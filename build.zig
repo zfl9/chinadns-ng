dec366a133a50d9dcb2154d59b9253f0c0ee8b8f
zfl9 <zfl9.com@gmail.com>
2024-05-12 15:12:11 +0800

    version: 2024.05.12

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ const Step = std.build.Step;
 const LibExeObjStep = std.build.LibExeObjStep;
 const OptionsStep = std.build.OptionsStep;
 
-const chinadns_version = "2024.04.30";
+const chinadns_version = "2024.05.12";
 
 var _b: *Builder = undefined;
 
