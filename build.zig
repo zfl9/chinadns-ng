e3adc134a9c08b2e969f4a10e11a998f9a524cf1
zfl9 <zfl9.com@gmail.com>
2024-07-20 16:03:17 +0800

    release: 2024.07.20

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ const Step = std.build.Step;
 const LibExeObjStep = std.build.LibExeObjStep;
 const OptionsStep = std.build.OptionsStep;
 
-const chinadns_version = "2024.07.16";
+const chinadns_version = "2024.07.20";
 
 var _b: *Builder = undefined;
 
