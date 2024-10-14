39d4881f83fa139b52cff9d8e306c4313bf758ad
zfl9 <zfl9.com@gmail.com>
2024-10-14 17:54:42 +0800

    wolfssl: 5.7.2

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ const Step = std.build.Step;
 const LibExeObjStep = std.build.LibExeObjStep;
 const OptionsStep = std.build.OptionsStep;
 
-const chinadns_version = "2024.09.08";
+const chinadns_version = "2024.10.14";
 
 var _b: *Builder = undefined;
 
@@ -37,7 +37,7 @@ const DependLib = struct {
 };
 
 var _dep_wolfssl: DependLib = b: {
-    const version = "5.7.0";
+    const version = "5.7.2";
     const src_dir = "dep/wolfssl-" ++ version;
     break :b .{
         .url = "https://github.com/wolfSSL/wolfssl/archive/refs/tags/v" ++ version ++ "-stable.tar.gz",
